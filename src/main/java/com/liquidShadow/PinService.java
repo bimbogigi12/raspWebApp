761bacb86c110660eeb030b0a98d922aa17a8db0
Maurizio Grassi <m.grassi@reply.it>
2022-03-03 14:59:01 +0100

    first commit

--- src/main/java/com/liquidShadow/PinService.java
+++
@@ -1,9 +0,0 @@
-package com.liquidShadow;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface PinService {
-	public int readPinValue(int pin);
-	public void setPinValue(int pin, int value);
-}
