7918eae903b8aa3aefdb9f46664e1c7cc8b4c45b
Maurizio Grassi <m.grassi@reply.it>
2022-03-03 15:09:18 +0100

    first commit

---
+++ src/main/java/com/liquidShadow/PinController.java
@@ -0,0 +1,31 @@
+package com.liquidShadow;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/pin")
+public class PinController {
+
+	
+	@Autowired
+	PinService pinService;
+	
+	@RequestMapping("/read/{pinId}")
+    public ResponseEntity<Integer> readPinValue (@PathVariable("pinId") Integer pinId) {
+        Integer pinValue = pinService.readPinValue(pinId);
+        
+        return new ResponseEntity<>(pinValue, HttpStatus.OK);
+    }
+    
+	@RequestMapping("/set/{pinId}/{value}")
+    public ResponseEntity<String> setPinValue (@PathVariable("pinId") Integer pinId, @PathVariable("value") Integer value) {
+        pinService.setPinValue(pinId, value);
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+}
